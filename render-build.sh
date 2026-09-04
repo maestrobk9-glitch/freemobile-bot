2b39ff274a0b7ffb3c6a626e57c10a1b7d08a5fd
maestrobk9-glitch <maestrobk9@gmail.com>
2026-09-04 02:43:08 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,4 +3,7 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# تحديد مسار ثابت لتثبيت متصفح Playwright لتجنب أي أخطاء في المسارات
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/pw-browsers
 playwright install chromium
